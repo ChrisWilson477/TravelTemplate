d92e8bead3ef7b50640db52a1c12e0fe9f162aed
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-01-11 16:45:43 -0700

    Template done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import react from 'react';
+
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
@@ -12,12 +13,13 @@ function App() {
     <>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/services' exact component={Services}/>
-          <Route path='/products' exact component={Products}/>
-          <Route path='/sign-up' exact component={SignUp}/>
-        </Switch>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/services' exact component={Services}/>
+            <Route path='/products' exact component={Products}/>
+            <Route path='/sign-up' exact component={SignUp}/>
+          </Switch>
+        <Footer />
       </Router>
     </>
   );

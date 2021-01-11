02a0950613569bdc396a3cf229835bf6016f2f3f
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-01-11 15:21:10 -0700

    Cards created, Additional routes created

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import react from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
+import Cards from '../Cards';
 
 function Home () {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   )
 }
